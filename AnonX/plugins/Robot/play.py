10e254b1f7f4be4faafe5ba9aadf8273a58e9cef
WE773533700 <123242811+WE773533700@users.noreply.github.com>
2023-02-21 18:22:29 +0300

    Add files via upload

--- AnonX/plugins/Robot/play.py
+++ AnonX/plugins/Robot/play.py
@@ -26,6 +26,46 @@ from AnonX.utils.inline.playlist import botplaylist_markup
 from AnonX.utils.logger import play_logs
 from AnonX.utils.stream.stream import stream
 
+
+force_btn = InlineKeyboardMarkup(
+
+    [
+
+        [
+
+            InlineKeyboardButton(
+
+                text="قناة البوت", url=f"{CHANNEL_SUDO}"
+
+            ),                        
+
+        ],        
+
+    ]
+
+)
+
+
+
+async def check_is_joined(message):    
+
+    try:
+
+        userid = message.from_user.id
+
+        status = await app.get_chat_member(f"{CHANNEL}", userid)
+
+        return True
+
+    except Exception:
+
+        await message.reply_text( "**◇︰ عذرا، عليك لاشتراك في قناة البوت أولاً." ,reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+
+        return False
+        
+        
+        
+        
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
